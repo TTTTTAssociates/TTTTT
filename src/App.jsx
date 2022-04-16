5d47a8eaad9dc38f91d7a4411b89032141746e6d
ttttt <tttttwister@yandex.ru>
2022-04-16 13:59:33 -0700

    Play Sound
    Fuck React CENSORING ME, FREE SPEECH?!

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import logo from './GABE.png';
 import music from './TTTTT.mp3';
 import Sound from 'react-sound';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [playStatus, setPlayStatus] = useState('STOPPED');
+
+  const playMusic = () => setPlayStatus('PLAYING');
+
   return (
-    <div className="App">
+    <div className="App" onClick={playMusic} onMouseMove={playMusic}>
       <header className="App-header">
         <h1>TTTTT</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>CRAFTERS.</p>
       </header>
-      <Sound url={music} playStatus={'PLAYING'}/>
+      <Sound url={music} playStatus={playStatus}/>
     </div>
   );
 }
