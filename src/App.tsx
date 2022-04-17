dd8e313e214da71352d6b510fd7740c4e96f3508
ttttt <tttttwister@yandex.ru>
2022-04-16 23:03:01 -0700

    Add Complaints Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import { useState } from "react";
 import Sound from "react-sound";
 import "./App.css";
+import { UnlickableButton } from "./buttons/UnlickableButton";
 import logo from "./media/PROPERLLERHEAD.png";
 import music from "./media/TTTTT.mp3";
 
@@ -14,11 +15,10 @@ function App() {
 
   return (
     <div className="App" onClick={playMusic}>
-      <header className="App-header">
-        <h1>TTTTT</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>CRAFTERS.</p>
-      </header>
+      <h1>TTTTT</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>CRAFTERS.</p>
+      <UnlickableButton text={"Complaints? Click Here"} />
       <Sound url={music} playStatus={playStatus} />
     </div>
   );
---
+++ src/buttons/UnlickableButton.tsx
@@ -0,0 +1,52 @@
+import { MouseEvent, useState } from "react";
+
+type UnclickableButtonProps = {
+  text: string;
+};
+
+const getNumberInBound = (
+  original: number,
+  bound: number,
+  lowLimit: number,
+  highLimit: number
+): number => {
+  const number =
+    (Math.floor(Math.random() * bound) + 50) * (Math.random() < 0.5 ? -1 : 1);
+  const newNumber = original + number;
+  if (newNumber < lowLimit || newNumber > highLimit) {
+    return original - number;
+  }
+  return newNumber;
+};
+
+export const UnlickableButton = (props: UnclickableButtonProps) => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [buttonPosition, setButtonPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: MouseEvent) => {
+    setMousePosition({ x: event.pageX, y: event.pageY });
+  };
+
+  const runAway = () => {
+    setButtonPosition({
+      x: getNumberInBound(mousePosition.x, 50, 0, window.outerHeight),
+      y: getNumberInBound(mousePosition.y, 50, 0, window.outerHeight),
+    });
+  };
+
+  return (
+    <div onMouseMove={handleMouseMove}>
+      <button
+        onClick={runAway}
+        onMouseOver={runAway}
+        style={{
+          left: `${buttonPosition.x}px`,
+          top: `${buttonPosition.y}px`,
+          position: "fixed",
+        }}
+      >
+        {props.text}
+      </button>
+    </div>
+  );
+};
