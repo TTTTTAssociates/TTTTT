2ea969ddb0eac10014d6ea5741364446ed5b2158
ttttt <tttttwister@yandex.ru>
2022-04-27 23:52:56 -0700

    Switch to HashRouter from BrowserRouter
    Github pages does not support sub pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { Route } from "react-router";
 import { HomePage } from "./home/HomePage";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { HashRouter, Routes } from "react-router-dom";
 import { NavigationBar } from "./navigation/NavigationBar";
 import { MissionStatement } from "./mission/MissionStatement";
 
@@ -9,12 +9,12 @@ export const App = () => {
   return (
     <div>
     <NavigationBar/>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/mission" element={<MissionStatement />}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </div>
   );
 }
