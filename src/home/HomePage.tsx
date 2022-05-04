cebbfc05f9aaf37253dd648990bcfa9314b9f27b
ttttt <tttttwister@yandex.ru>
2022-05-03 17:04:40 -0700

    Add Draggables

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -5,6 +5,7 @@ import { UnlickableButton } from "../buttons/UnlickableButton";
 import logo from "../media/PROPERLLERHEAD.png";
 import music from "../media/TTTTT.mp3";
 import "./HomePage.css";
+import Draggable from "react-draggable";
 
 export const HomePage = () => {
     const [playStatus, setPlayStatus] = useState<
@@ -14,10 +15,21 @@ export const HomePage = () => {
   const playMusic = () => setPlayStatus("PLAYING");
 
     return <div className="HomePage" onClick={playMusic}>
-    <h1>TTTTT</h1>
-    <img src={logo} className="HomePage-logo" alt="logo" />
-    <p>CRAFTERS.</p>
+    <Draggable>
+      <h1>TTTTT</h1>      
+    </Draggable>
+    <Draggable>
+      <div>
+        <img src={logo} className="HomePage-logo" alt="logo" />
+      </div>
+    </Draggable>
+    <Draggable>
+      <p>CRAFTERS.</p>
+    </Draggable>
     <UnlickableButton text={"Complaints? Click Here"} />
+    <Draggable>
+      <div className="Quote"/>     
+    </Draggable>    
     <Sound url={music} playStatus={playStatus} loop={true}/>
   </div>
 }
